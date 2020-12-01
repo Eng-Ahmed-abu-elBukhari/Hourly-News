9b298805d022ee9b880233bcc67d4ab06eec9883
Eng-Ahmed-abu-elBukhari <engahmedbukhari@gmail.com>
2020-12-01 14:30:47 +0200

    notify all

--- app/src/main/java/com/example/bukhari/hourly_news/MainActivity.java
+++ app/src/main/java/com/example/bukhari/hourly_news/MainActivity.java
@@ -1,14 +1,18 @@
 package com.example.bukhari.hourly_news;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
