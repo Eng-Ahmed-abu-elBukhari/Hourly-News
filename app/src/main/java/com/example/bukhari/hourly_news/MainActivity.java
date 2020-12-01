eed35c53c27302362bcfda704bce26afce50639f
Eng-Ahmed-abu-elBukhari <engahmedbukhari@gmail.com>
2020-12-01 15:41:30 +0200

    last

--- app/src/main/java/com/example/bukhari/hourly_news/MainActivity.java
+++ app/src/main/java/com/example/bukhari/hourly_news/MainActivity.java
@@ -7,7 +7,7 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
 
-    private Button button;
+    public Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
